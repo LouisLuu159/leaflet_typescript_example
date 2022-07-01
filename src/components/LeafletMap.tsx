c03343dc0a1ec8e4b27e493729b0f81491ad8702
LouisLuu159 <52624237+LouisLuu159@users.noreply.github.com>
2022-07-01 17:52:18 +0700

    Fix save coordinate circle and marker

--- src/components/LeafletMap.tsx
+++ src/components/LeafletMap.tsx
@@ -1,5 +1,11 @@
 import L from "leaflet";
-import { TileLayer, FeatureGroup, MapContainer, useMap } from "react-leaflet";
+import {
+  TileLayer,
+  FeatureGroup,
+  MapContainer,
+  useMap,
+  Rectangle,
+} from "react-leaflet";
 import { EditControl } from "react-leaflet-draw";
 import "leaflet/dist/leaflet.css";
 import "leaflet-draw/dist/leaflet.draw.css";
@@ -21,24 +27,80 @@ export const LeafletMap = () => {
   let initEditableFG = new L.FeatureGroup<any>();
   const [editableFG, setEditableFG] = useState(initEditableFG);
 
+  enum ShapeType {
+    RECTANGLE = "rectangle",
+    CIRCLE = "circle",
+    MARKER = "marker",
+    POLYGON = "polygon",
+  }
+
+  interface Item {
+    latlngs: { lat: number; lng: number }[];
+    type: ShapeType;
+    mRadius?: number;
+  }
+
+  let initDrawnItems: {
+    [id: string]: Item;
+  } = {};
+  const [drawnItems, setDrawnItems] = useState(initDrawnItems);
+
   const onCreated = (e: any) => {
     console.log(typeof e.layer);
+
+    const layer = e.layer;
     editableFG.addLayer(e.layer);
 
-    console.log("Created");
-    const layers = editableFG.getLayers();
-    console.log(layers);
+    console.log(Object.keys(e));
+    console.log(layer);
 
-    let drawnItems: {
-      [id: string]: { lat: number; lng: number }[];
-    } = {};
+    const layerType = e.layerType;
+    let item: Item = { latlngs: [], type: ShapeType.RECTANGLE };
 
-    layers.forEach((layer: any) => {
-      //   console.log(Object.keys(layer));
-      console.log(layer._latlngs);
-      drawnItems[layer._leaflet_id] = layer._latlngs[0];
-    });
+    switch (layerType) {
+      case ShapeType.POLYGON: {
+        item.type = ShapeType.POLYGON;
+        item.latlngs = layer._latlngs[0];
+        break;
+      }
+
+      case ShapeType.RECTANGLE: {
+        item.type = ShapeType.RECTANGLE;
+        item.latlngs = layer._latlngs[0];
+        break;
+      }
+
+      case ShapeType.CIRCLE: {
+        item.mRadius = layer._mRadius;
+        item.type = ShapeType.CIRCLE;
+        item.latlngs = [layer._latlng];
+        break;
+      }
+
+      case ShapeType.MARKER: {
+        item.type = ShapeType.MARKER;
+        item.latlngs = [layer._latlng];
+        break;
+      }
+    }
+    drawnItems[layer._leaflet_id] = item;
     console.log(JSON.stringify(drawnItems));
+    setDrawnItems(drawnItems);
+
+    // drawnItems[layer._leaflet_id] = layer._latlngs[0];
+
+    // editableFG.addLayer(e.layer);
+
+    // console.log("Created");
+    // const layers = editableFG.getLayers();
+    // console.log(layers);
+
+    // layers.forEach((layer: any) => {
+    //   //   console.log(Object.keys(layer));
+    //   //   console.log(layer._latlngs);
+    //   //   drawnItems[layer._leaflet_id] = layer._latlngs[0];
+    // });
+    // console.log(JSON.stringify(drawnItems));
   };
 
   const onFeatureGroupReady = (reactFGref: any) => {
