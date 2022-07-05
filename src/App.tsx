99c49dc7322a941de330546a46da8a5b8430818c
LouisLuu159 <52624237+LouisLuu159@users.noreply.github.com>
2022-07-05 14:33:49 +0700

    Add choropleth map

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "./App.css";
-import { LeafletMap } from "./components/LeafletMap";
+import { ChoroplethMap } from "./components/ChoroplethMap";
+// import { LeafletMap } from "./components/LeafletMap";
 import { AppProvider } from "./context/AppProvider";
 
 function App() {
   return (
     <AppProvider>
       <div className="App">
-        <LeafletMap />
+        <ChoroplethMap />
       </div>
     </AppProvider>
   );
--- src/components/ChoroplethMap.tsx
+++ src/components/ChoroplethMap.tsx
@@ -1,37 +1,185 @@
-import { useContext } from "react";
-import { MapContainer, TileLayer, useMap } from "react-leaflet";
+import L from "leaflet";
+import { TileLayer, MapContainer, useMap } from "react-leaflet";
+import { EditControl } from "react-leaflet-draw";
+import "leaflet/dist/leaflet.css";
+import "leaflet-draw/dist/leaflet.draw.css";
+import { useContext, useDebugValue, useEffect, useRef, useState } from "react";
 import { AppContext } from "../context/AppProvider";
-import stateData from "../data/us_state.json";
-import { Control, DomUtil } from "leaflet";
-import L from 'leaflet';
 
-export const ChoroplethMap = () => {
-  const map = useMap();
+import GeoJSON from "geojson";
+import statesData from "../data/us_state.json";
+import { updateSourceFile } from "typescript";
 
-  const ctx = useContext(AppContext);
+const getColor = (d: number): string => {
+  return d > 1000
+    ? "#800026"
+    : d > 500
+    ? "#BD0026"
+    : d > 200
+    ? "#E31A1C"
+    : d > 100
+    ? "#FC4E2A"
+    : d > 50
+    ? "#FD8D3C"
+    : d > 20
+    ? "#FEB24C"
+    : d > 10
+    ? "#FED976"
+    : "#FFEDA0";
+};
 
-//   L.Control.Watermark = Control.extend({
-//     onAdd: function (map) {
-//       const img = DomUtil.create("img");
-//       img.src = "./logo.png";
-//       img.style.width = "200px";
-//       return img;
-//     },
-
-//     onRemove: function (map) {},
-//   });
+const Legend = () => {
+  let grades = [0, 10, 20, 50, 100, 200, 500, 1000];
 
   return (
-    <MapContainer
-      center={ctx?.center}
-      zoom={ctx?.zoomLevel}
-      minZoom={ctx?.minZoom}
-      maxZoom={ctx?.maxZoom}
-    >
+    <div className="leaflet-bottom leaflet-right">
+      <div className="leaflet-control leaflet-bar info legend">
+        {grades.map((grade, index) => {
+          let grade_txt = `${grade}`;
+          if (grades[index + 1]) grade_txt += `-${grades[index + 1]}`;
+          else grade_txt += "+";
+
+          return (
+            <div className="grade">
+              <i style={{ background: getColor(grades[index + 1]) }}></i>
+              {grade_txt}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+const Info = () => {
+  const map = useMap();
+
+  let info_div: HTMLDivElement;
+
+  const [info, setInfo] = useState<{ name: string; density: string } | null>(
+    null
+  );
+
+  const update = (props?: any) => {
+    console.log(props);
+
+    if (props) setInfo({ name: props.name, density: props.density });
+    else setInfo(null);
+  };
+
+  const onAdd = (map?: L.Map) => {
+    info_div = L.DomUtil.create("div", "info");
+    update();
+  };
+
+  const InfoControl = L.Control.extend({
+    onAdd: onAdd,
+    update: update,
+  });
+
+  // get color depending on population density value
+
+  const style = (feature: any) => {
+    return {
+      weight: 2,
+      opacity: 1,
+      color: "white",
+      dashArray: "3",
+      fillOpacity: 0.7,
+      fillColor: getColor(feature.properties.density),
+    };
+  };
+
+  function zoomToFeature(e: any) {
+    map.fitBounds(e.target.getBounds());
+  }
+
+  useEffect(() => {
+    const infoControl = new InfoControl();
+    console.log("Render");
+
+    infoControl.onAdd();
+
+    const highlightFeature = (e: any) => {
+      var layer = e.target;
+
+      layer.setStyle({
+        weight: 5,
+        color: "#666",
+        dashArray: "",
+        fillOpacity: 0.7,
+      });
+
+      if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+      }
+
+      infoControl.update(layer.feature.properties);
+    };
+
+    let geoJson: L.GeoJSON<any>;
+
+    const resetHighlight = (e: any) => {
+      geoJson.resetStyle(e.target);
+      infoControl.update();
+    };
+
+    const onEachFeature = (feature: any, layer: any) => {
+      layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature,
+      });
+    };
+
+    const data = statesData as GeoJSON.FeatureCollection<GeoJSON.Geometry>;
+
+    geoJson = L.geoJSON(data, {
+      style: style,
+      onEachFeature: onEachFeature,
+    });
+
+    geoJson.addTo(map);
+  }, [map]);
+
+  return (
+    <div className="leaflet-top leaflet-right">
+      <div className="info leaflet-control leaflet-bar">
+        <h4>US Population Density</h4>
+        {info ? (
+          <>
+            <b> {info.name}</b>
+            <br />
+            {info.density} people / mi<sup>2</sup>
+          </>
+        ) : (
+          <>Hover over a state</>
+        )}
+      </div>
+    </div>
+  );
+};
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon.png",
+  iconUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-shadow.png",
+});
+
+export const ChoroplethMap = () => {
+  const ctx = useContext(AppContext);
+
+  return (
+    <MapContainer center={[37.8, -96]} maxZoom={19} zoom={4}>
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
       />
+      <Info />
+      <Legend />
     </MapContainer>
   );
 };
